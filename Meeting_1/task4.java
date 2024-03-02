9e89dde6e6eb86fa636d766cb86506d376b79e92
Jakub Zlotek <kubazlotek@gmail.com>
2024-03-02 10:10:25 +0100

    task 4 and 5

---
+++ Meeting_1/task4.java
@@ -0,0 +1,9 @@
+public class task4 {
+  public static void main(String[] args) {
+    double weight = 60;
+    double height = 1.7;
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is: " + bmi);
+  }
+}
+
---
+++ Meeting_1/task5.java
@@ -0,0 +1,31 @@
+public class task5 {
+  public static void main(String[] args) {
+    int[] gradeArray = {3,4,5,3};
+    double sum = 0;
+    for (int i = 0; i<gradeArray.length; i++) {
+      sum+=gradeArray[i];
+    }
+    double gradeAverage = sum/gradeArray.length;
+    if (gradeAverage >= 4.75) {
+        System.out.println("A+");
+    } else if (gradeAverage >= 4.5) {
+        System.out.println("A");
+    } else if (gradeAverage >= 4.0) {
+        System.out.println("A-");
+    } else if (gradeAverage >= 3.5) {
+        System.out.println("B+");
+    } else if (gradeAverage >= 3.0) {
+        System.out.println("B");
+    } else if (gradeAverage >= 2.5) {
+        System.out.println("B-");
+    } else if (gradeAverage >= 2.0) {
+        System.out.println("C+");
+    } else if (gradeAverage >= 1.5) {
+        System.out.println("C");
+    } else if (gradeAverage >= 1.0) {
+        System.out.println("C-");
+    } else {
+        System.out.println("F");
+    }
+  }
+}
