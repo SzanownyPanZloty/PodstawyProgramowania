a444b2b773f986b41f26d3fa679af202722cfdfd
Jakub Zlotek <kubazlotek@gmail.com>
2024-03-02 11:43:27 +0100

    task 6

---
+++ Meeting_1/task6.java
@@ -0,0 +1,17 @@
+public class task6 {
+  public static void main(String[] args) {
+    String word = "kajak";
+    if (isPalindrome(word)) {
+      System.out.println("Word: " + word + " is palindrome");
+    } else {
+      System.out.println("Word: " + word + " is not palindrome");
+    }
+
+  }
+  public static boolean isPalindrome(String baseWord) {
+    String reversedWord = new StringBuilder(baseWord)
+      .reverse()
+      .toString();
+    return reversedWord.equals(baseWord) ? true : false;
+  }
+}
