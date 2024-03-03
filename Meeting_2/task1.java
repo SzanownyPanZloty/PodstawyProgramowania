f46c915f42bb1a866f87d6329d7ea83ca11767c0
Jakub Zlotek <kubazlotek@gmail.com>
2024-03-03 09:02:51 +0100

    Created leniwiec.bat

---
+++ Meeting_2/task1.java
@@ -0,0 +1,7 @@
+package Meeting_2;
+
+public class task1 {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ leniwiec.bat
@@ -0,0 +1,4 @@
+::Zgadza sie, jestem zbyt leniwy by napisac 3 polecenia :)
+git add .
+git commit -m "%*"
+git push --force
